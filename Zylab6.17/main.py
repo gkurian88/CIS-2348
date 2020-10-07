9fea073c8c6f61a826abe0acc985e3d89d8b818e
gkurian88 <70449630+gkurian88@users.noreply.github.com>
2020-10-06 23:00:29 -0500

    Create main.py

---
+++ Zylab6.17/main.py
@@ -0,0 +1,24 @@
+word = input()
+password = ''
+
+
+i = 0
+while i < len(word):
+    ch = word[i]
+    if ch == 'i':
+        password += '!'
+    elif ch == 'a':
+        password += '@'
+    elif ch == 'm':
+        password += 'M'
+    elif ch == 'B':
+        password += '8'
+    elif ch == 'o':
+        password += '.'
+    else:
+        password += ch
+    i += 1
+
+password += "q*s"
+
+print(password)
