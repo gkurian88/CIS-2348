4823d17ae7648d8877dd06a42e4359dde6a0721c
gkurian88 <70449630+gkurian88@users.noreply.github.com>
2020-10-06 23:05:30 -0500

    Create main.py

---
+++ Zylab8.10/main.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    user_input = input()
+    normal = ""
+    reverse = ""
+    for ch in user_input.lower():
+        if ch != ' ':
+            normal += ch
+            reverse = ch + reverse
+    if normal == reverse:
+        print(user_input + " is a palindrome")
+    else:
+        print(user_input + " is not a palindrome")
