f5be167b05bbe243996e8cc160bb03cdf33d5f88
gkurian88 <70449630+gkurian88@users.noreply.github.com>
2020-10-06 22:58:22 -0500

    Create main.py

---
+++ homework1/main.py
@@ -0,0 +1,22 @@
+#CIS 2348 Homework 1, Fall 2020
+#Coding Problem 1
+
+
+print('Birthday Calculator')
+print('Current day')
+m1 = int(input('Month: '))
+d1 = int(input('Day: '))
+y1 = int(input('Year: '))
+print('Birthday')
+m2 = int(input('Month: '))
+d2 = int(input('Day: '))
+y2 = int(input('Year: '))
+years = y1-y2-1
+if(m2<m1):
+   years+=1
+elif(m1==m2):
+   if(d2<d1):
+      years+=1
+if(m1==m2 and d1==d2):#printing happy Birthday if current day is his birthday
+   print('Happy Birthay')
+print('You are '+str(years)+" years old.")
