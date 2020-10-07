7024627e1d77c6d60ba1b2cc7465ca1a3cf66b19
gkurian88 <70449630+gkurian88@users.noreply.github.com>
2020-10-06 22:47:12 -0500

    Create main.py

---
+++ Zylab1.20/main.py
@@ -0,0 +1,15 @@
+user_num = int(input('Enter integer:\n'))
+
+# Type your code here
+print('You entered:',user_num)
+x=user_num**2
+print(user_num,'squared is',x)
+y=user_num**3
+print('And',user_num,'cubed is',y,'!!')
+a=int(input('Enter another integer:\n'))
+
+s=user_num+a
+t=user_num*a
+
+print(user_num,'+',a,'is',s)
+print(user_num,'*',a,'is',t)
