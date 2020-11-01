be2b66fdf15b8c5facec3c84092f71aa50a533d6
gkurian88 <70449630+gkurian88@users.noreply.github.com>
2020-11-01 14:32:17 -0600

    Update main.py

--- Zybook11.27/main.py
+++ Zybook11.27/main.py
@@ -1,109 +1,98 @@
 #Githin Kurian
 #1580561
 
-def printRoster():
-   # get all players jersey numbers
-   keys = list(players.keys())
+def roster():
+   keys = list(ply.keys())
 
-   # sort jersey numbers in ascending order
    keys.sort()
 
-   # print roster
    print("ROSTER")
-   for key in keys:
-       print("Jersey number: %d, Rating: %d" %(key,players[key]))
+   for i in keys:
+       print("Jersey number: %d, Rating: %d" %(i,ply[i]))
 
-# define a dictionary to store players jersey number as keys and rating as values
-players = {}
+ply = {}
 
-# iterate for loop 5 times
 for i in range(5):
-   # ask user to enter player (i+1)'s jersey number and read
+ 
    jno = int(input("Enter player %d's jersey number:\n" %(i+1)))
   
-   # ask user to enter player (i+1)'s rating and read
-   players[jno] = int(input("Enter player %d's rating:\n" %(i+1)))
+   ply[jno] = int(input("Enter player %d's rating:\n" %(i+1)))
   
-   # print new line
    print("")
   
 
 
-printRoster()
+roster()
 
-# iterate while loop
 while True:
 
-   # print menu
-   print("\nMENU\na - Add player\nd - Remove player\nu - Update player rating\nr - Output players above a rating\no - Output roster\nq - Quit\n")
+   
+   print('\nMENU\na - Add player')
+   print('d - Remove player')
+   print('u - Update player rating')
+   print('r - Output players above a rating')
+   print('o - Output roster')
+   print('q - Quit\n')
   
-   # read user option
-   option = input("Choose an option:\n")
+   op = input("Choose an option:\n")  
   
-  
-   # if user option is o call printRoster() function to print roster
-   if option == 'o':
-       printRoster()
-      
-   # if user option is 'a'
-   elif option == 'a':
-       # read new player's jersey number
+   if op == 'o':
+       roster()
+    
+   elif op == 'a':
+
        jno = int(input("Enter a new player's jersey number: \n"))
       
-       # read new player's rating
        rating = int(input("Enter the player's rating:\n"))
       
-       # add player details into players dictionary
-       players[jno] = rating
+
+       ply[jno] = rating
   
-   # if user option is 'd'
-   elif option == 'd':
-       # read a player's jersey number
+   elif op == 'd':
+      
        jno = int(input("Enter a player's jersey number: \n"))
       
-       # check a player with the given jersey number is present in the dictionary
-       if jno in list(players.keys()):
-           # delete player
-           del players[jno]
+
+       if jno in list(ply.keys()):
+       
+           del ply[jno]
   
-   # if user option is 'u'
-   elif option == 'u':
-       # read a player's jersey number
+
+   elif op == 'u':
+
        jno = int(input("Enter a player's jersey number: \n"))
       
-       # read new player's rating
+       
        rating = int(input("Enter a new rating for player:\n"))
       
-       # update player details into players dictionary
-       players[jno] = rating
+       ply[jno] = rating
       
-   elif option == 'r':
-       # read a rating
+   elif op == 'r':
+      
        rating = int(input("Enter a rating:\n"))
       
-       # get all players jersey numbers
-       keys = list(players.keys())
+      
+       keys = list(ply.keys())
 
-       # sort jersey numbers in ascending order
+   
        keys.sort()
 
-       # print players above given rating
        print("\nABOVE %d" %(rating))
       
-       count = 0
-       for key in keys:
-           # print player's jersey number, and rating
-           if(players[key]>rating):
-               print("Jersey number: %d, Rating: %d" %(key,players[key]))
+       c = 0
+       for i in keys:
+          
+           if(ply[i]>rating):
+               print("Jersey number: %d, Rating: %d" %(i,ply[i]))
               
-               # increment count by 1
-               count +=1
+               c +=1
       
-       # if no players have above given rating, print error message
-       if(count == 0):
+       if(c == 0):
            print("No players found above %d rating" %(rating))
       
-  
-   # if user option is q then stop the program
-   if option == "q":
+
+   if op == "q":
        break
+
+   
+  
