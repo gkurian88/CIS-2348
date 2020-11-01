44db8498cfde95c31857c748c3e6d1a460d2eb8e
gkurian88 <70449630+gkurian88@users.noreply.github.com>
2020-11-01 14:27:26 -0600

    Update main.py

--- Zylab10.11/main.py
+++ Zylab10.11/main.py
@@ -2,18 +2,17 @@
 #1580561
 
 class FoodItem:
-    # TODO: Define constructor with parameters to initialize instance 
-    #       attributes (name, fat, carbs, protein)
-    def __init__(self, name="None", fat=0.0, carbs=0.0, protein=0.0):
+
+    def __init__(self, name="None", fat=0.0, carb=0.0, protein=0.0):
         self.name = name
         self.fat = fat
-        self.carbs = carbs
+        self.carbs = carb
         self.protein = protein
 
        
-    def get_calories(self, num_servings):
-        # Calorie formula
-        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings;
+    def get_calories(self, servings):
+     
+        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * servings;
         return calories
        
     def print_info(self):
@@ -24,24 +23,24 @@ class FoodItem:
 
 if __name__ == "__main__":
     
-    food_item1 = FoodItem()
+    fd1 = FoodItem()
    
     item_name = input()
     amount_fat = float(input())
     amount_carbs = float(input())
     amount_protein = float(input())
    
-    food_item2 = FoodItem(item_name, amount_fat, amount_carbs, amount_protein)
+    fd2 = FoodItem(item_name, amount_fat, amount_carbs, amount_protein)
       
-    num_servings = float(input())
+    servings = float(input())
       
-    food_item1.print_info()
-    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(num_servings, 
-                          food_item1.get_calories(num_servings)))
+    fd1.print_info()
+    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(servings, 
+                          fd1.get_calories(servings)))
                            
     print()
                            
-    food_item2.print_info()
-    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(num_servings, 
-                          food_item2.get_calories(num_servings)))
+    fd2.print_info()
+    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(servings, 
+                          fd2.get_calories(servings)))
     
